1c4555085c07c3e6b8b3f55c6b9e1ccae1213170
tung28022002 <kingrampage2002@gmail.com>
2022-03-10 21:38:13 +0700

    Initial commit

---
+++ F8/main.js
@@ -0,0 +1,14 @@
+hoTen=prompt("Nhập họ tên của bạn:");
+ngaySinh=prompt("Nhập ngày tháng năm sinh của bạn:");
+			
+            diemToan=prompt("Nhập điểm toán:");
+            diemLy=prompt("Nhập điểm lý:");
+            diemHoa=prompt("Nhập điểm hóa:");
+			email=prompt("Nhập email:");
+            alert("Tên của bạn là" + hoTen);
+            alert("Ngày sinh của bạn là" + ngaySinh);
+            alert("Điểm toán của bạn là" + diemToan);
+            alert("Điểm lý bạn là" + diemLy);
+            alert("Điểm hóa của bạn là" + diemHoa);
+            alert("Email của bạn là" + email);
+
